d7a852462a2c157759c843990efc4d1ce5dbbb19
Toni Moreira <moreiraatoni@gmail.com>
2024-04-30 15:49:30 -0300

    main commit

---
+++ main.py
@@ -0,0 +1,55 @@
+import os
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from time import sleep
+
+executable_path = "/Users/tonimoreira/Projects/contab-dte/chromedriver"
+os.environ["webdriver.chrome.driver"] = executable_path
+
+chrome_options = Options()
+chrome_options.add_extension('./Web-PKI.crx')
+
+driver = webdriver.Chrome(options=chrome_options)
+driver.get("https://dte.sefaz.al.gov.br/dte/login/?redirect=/dte/client/nucleo/nova-base/public/#/")
+
+selectButton = WebDriverWait(driver, 100).until(
+    EC.presence_of_element_located((By.XPATH, '//*[@id="root"]/div/div[2]/div/div/form/div[1]/div/div/div/div'))
+)
+
+selectButton.click()
+
+certs = WebDriverWait(driver, 100).until(
+    EC.presence_of_all_elements_located((By.XPATH, './/*[contains(concat(" ",normalize-space(@class)," ")," rc-virtual-list-holder-inner ")]/div'))
+)
+for cert in certs:
+    cert.click()
+    driver.find_element(By.XPATH, '//*[@id="root"]/div/div[2]/div/div/form/div[2]/div/div/div/button').click()
+    companies = WebDriverWait(driver, 100).until(
+        EC.presence_of_all_elements_located((By.CSS_SELECTOR, '.SelecionarContratoHonorario__List > div'))
+    )
+    for company in companies:
+        companyName = company.get_attribute('textContent')
+        company.click()
+        driver.find_element(By.CSS_SELECTOR, '.SelecionarContratoHonorario__Button').click()
+
+        # Espera carregar
+        iframe = WebDriverWait(driver, 100).until(
+            EC.presence_of_element_located((By.CSS_SELECTOR, '.Audora__Body > iframe'))
+        )
+        driver.switch_to.frame(iframe)
+        WebDriverWait(driver, 100).until(
+            EC.presence_of_element_located((By.XPATH , '/html/body/div[1]/div/div[3]/div/div[1]/a[2]/span'))
+        )
+        
+        # Printa
+        print('Screenshot')
+        screenshotPath = os.path.join('./output', companyName + '.png')
+        driver.get_screenshot_as_file(screenshotPath)
+
+
+        sleep(100)
+
+    sleep(100)
