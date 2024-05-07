4760ee7dfadf772c64faea09825b3d559fe49f29
Toni Moreira <moreiraatoni@gmail.com>
2024-05-07 11:26:26 -0300

    feat: support to all certs and companies

--- main.py
+++ main.py
@@ -24,13 +24,49 @@ selectButton.click()
 certs = WebDriverWait(driver, 100).until(
     EC.presence_of_all_elements_located((By.XPATH, './/*[contains(concat(" ",normalize-space(@class)," ")," rc-virtual-list-holder-inner ")]/div'))
 )
-for cert in certs:
+for certIndex in range(0, len(certs)):
+    if certIndex != 0:
+        # Faz logout
+        driver.delete_all_cookies()
+        driver.get("https://dte.sefaz.al.gov.br/dte/login/?redirect=/dte/client/nucleo/nova-base/public/#/")
+
+        selectButton = WebDriverWait(driver, 100).until(
+            EC.presence_of_element_located((By.XPATH, '//*[@id="root"]/div/div[2]/div/div/form/div[1]/div/div/div/div'))
+        )
+
+        selectButton.click()
+        certs = WebDriverWait(driver, 100).until(
+            EC.presence_of_all_elements_located((By.XPATH, './/*[contains(concat(" ",normalize-space(@class)," ")," rc-virtual-list-holder-inner ")]/div'))
+        )
+
+    cert = certs[certIndex]
     cert.click()
     driver.find_element(By.XPATH, '//*[@id="root"]/div/div[2]/div/div/form/div[2]/div/div/div/button').click()
     companies = WebDriverWait(driver, 100).until(
         EC.presence_of_all_elements_located((By.CSS_SELECTOR, '.SelecionarContratoHonorario__List > div'))
     )
-    for company in companies:
+    companiesLen = len(companies)
+    for companyIndex in range(0, companiesLen):
+        if companyIndex != 0:
+            # Faz logout
+            driver.delete_all_cookies()
+            driver.get("https://dte.sefaz.al.gov.br/dte/login/?redirect=/dte/client/nucleo/nova-base/public/#/")
+
+            selectButton = WebDriverWait(driver, 100).until(
+                EC.presence_of_element_located((By.XPATH, '//*[@id="root"]/div/div[2]/div/div/form/div[1]/div/div/div/div'))
+            )
+
+            selectButton.click()
+            certs = WebDriverWait(driver, 100).until(
+                EC.presence_of_all_elements_located((By.XPATH, './/*[contains(concat(" ",normalize-space(@class)," ")," rc-virtual-list-holder-inner ")]/div'))
+            )
+            certs[certIndex].click()
+            driver.find_element(By.XPATH, '//*[@id="root"]/div/div[2]/div/div/form/div[2]/div/div/div/button').click()
+            companies = WebDriverWait(driver, 100).until(
+                EC.presence_of_all_elements_located((By.CSS_SELECTOR, '.SelecionarContratoHonorario__List > div'))
+            )
+
+        company = companies[companyIndex]
         companyName = company.get_attribute('textContent')
         company.click()
         driver.find_element(By.CSS_SELECTOR, '.SelecionarContratoHonorario__Button').click()
@@ -46,10 +82,6 @@ for cert in certs:
         
         # Printa
         print('Screenshot')
+        sleep(2)
         screenshotPath = os.path.join('./output', companyName + '.png')
         driver.get_screenshot_as_file(screenshotPath)
-
-
-        sleep(100)
-
-    sleep(100)
